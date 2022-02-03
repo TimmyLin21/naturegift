0498d7e8a5695e7f08129734a3f1513bf3223c0e
TimmyLin21 <f7964032@gmail.com>
2022-02-03 16:24:18 +0800

    Adjust CategoryList

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,37 +1,45 @@
 <template lang="">
-  <section class="my-7">
+  <section class="py-7 position-relative overflow-hidden">
     <div class="py-5 bg-primary shadow-lg">
       <div class="container">
         <h2 class="mb-5 align-start text-secondary text-start">Shop by Category</h2>
         <ul class="nav flex-nowrap justify-content-between">
-          <!-- TODO: Add hover effect -->
-          <li class="card bg-dark text-white border-0 shadow">
-            <img src="https://images.unsplash.com/photo-1542990253-a781e04c0082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=794&q=80"
-            alt="nuts" class="card-img">
-            <div class="card-img-overlay">
-              <h3 class="card-text text-start">Nuts</h3>
+          <!-- FIXED: Shadow Effect -->
+          <li class="c-card text-white">
+            <figure class="c-card__img">
+              <img src="https://images.unsplash.com/photo-1542990253-a781e04c0082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=794&q=80"
+              alt="nuts">
+            </figure>
+            <div class="c-card__content">
+              <h3 class="text-start">Nuts</h3>
             </div>
             <a href="#" class="stretched-link"></a>
           </li>
-          <li class="card bg-dark text-white mx-5 border-0 shadow">
-            <img src="https://images.unsplash.com/photo-1537516825040-16759cfde671?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="nuts" class="card-img">
-            <div class="card-img-overlay">
-              <h3 class="card-text text-start">Beans</h3>
+          <li class="c-card text-white mx-5">
+            <figure class="c-card__img">
+              <img src="https://images.unsplash.com/photo-1537516825040-16759cfde671?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              alt="nuts">
+            </figure>
+            <div class="c-card__content">
+              <h3 class="text-start">Beans</h3>
             </div>
             <a href="#" class="stretched-link"></a>
           </li>
-          <li class="card bg-dark text-white border-0 shadow">
-            <img src="https://images.unsplash.com/photo-1596040033229-a9821ebd058d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            alt="nuts" class="card-img">
-            <div class="card-img-overlay">
-              <h3 class="card-text text-start">Spices</h3>
+          <li class="c-card text-white">
+            <figure class="c-card__img">
+              <img src="https://images.unsplash.com/photo-1596040033229-a9821ebd058d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              alt="nuts">
+            </figure>
+            <div class="c-card__content">
+              <h3 class="text-start">Spices</h3>
             </div>
             <a href="#" class="stretched-link"></a>
           </li>
         </ul>
       </div>
     </div>
+    <img src="@/assets/images/pine-branch.png" alt="pine-branch"
+    class="position-absolute category__decor rotate-n20">
   </section>
 </template>
 <script>
@@ -40,4 +48,9 @@ export default {
 };
 </script>
 <style lang="scss">
+  .category__decor {
+    height: 23.75rem;
+    top: 1.25rem;
+    right: -5.625rem;
+  }
 </style>
